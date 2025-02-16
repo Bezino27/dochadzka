b1e6fa25a3f4dabf4f5a93e7c11d5b3e81b7a24c
kurri <tomikbez@gmail.com>
2025-02-16 15:50:54 +0100

    procfile, requirements

--- dochadzka_backend/settings.py
+++ dochadzka_backend/settings.py
@@ -24,10 +24,20 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1w5z!l#w17qwi-ft4*08_3++sxuw81=)#m!r)7@c!+w9qwzp=n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['dochadzka.onrender.com', 'localhost', '127.0.0.1']
 
+# Kde sa budú ukladať statické súbory pri deployi
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# URL cesta pre statické súbory
+STATIC_URL = '/static/'
+
+# Miesta, kde Django hľadá statické súbory
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Application definition
 
