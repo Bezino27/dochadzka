ec1a88aeb817405bae2761742a994da4688df340
kurri <tomikbez@gmail.com>
2025-02-18 16:32:34 +0100

    procfile, requirements

--- dochadzka_backend/settings.py
+++ dochadzka_backend/settings.py
@@ -27,7 +27,9 @@ SECRET_KEY = 'django-insecure-1w5z!l#w17qwi-ft4*08_3++sxuw81=)#m!r)7@c!+w9qwzp=n
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
+CSRF_TRUSTED_ORIGINS = [
+    "https://web-databaseurl-5e2d.up.railway.app",
+]
 ALLOWED_HOSTS = ['dochadzka.onrender.com', 'localhost', '127.0.0.1', 'web-databaseurl-5e2d.up.railway.app']
 
 # Kde sa budú ukladať statické súbory pri deployi
